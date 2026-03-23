63ecba55da01b0856b891185674f4f2635448da9
BrionesAngel <14tenshikun14@gmail.com>
2026-03-23 17:28:15 -0600

    example events modifiers and bindinds added

--- section2_basics_&_core_concepts/basics-03-events-starting-code/app.js
+++ section2_basics_&_core_concepts/basics-03-events-starting-code/app.js
@@ -1,8 +1,32 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: '',
+      nameWithLastname: '',
+      confirmedName: ''
     };
+  },
+  methods: {
+    confirmInput(){
+      this.confirmedName = this.name
+    },
+    submitForm(event){
+      //event.preventDefault();
+      alert('Submitted!');
+    },
+    setName(event){
+      this.name = event.target.value;
+    },
+    setName_and_lastName(event, lastName){
+      this.nameWithLastname = event.target.value + ' ' + lastName;
+    },
+    add(num){
+      this.counter+=num;
+    },
+    reduce(){
+      this.counter--;
+    }
   },
 });
 
