0ccabe00f982058f32fe810122f39f413cb160cd
BrionesAngel <14tenshikun14@gmail.com>
2026-03-23 21:59:48 -0600

    assignment problem 3

---
+++ section2_basics_&_core_concepts/basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  computed: {
+    fullname() {
+      console.log('running again..');
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'lastname';
+    }
+  },
+  methods: {
+    outputFullname() {
+      console.log('running again..');
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'lastname';
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
