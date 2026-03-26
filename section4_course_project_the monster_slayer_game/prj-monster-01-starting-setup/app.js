c3000ba3c4346d71bd503311b5dca70f77923df0
BrionesAngel <14tenshikun14@gmail.com>
2026-03-26 00:19:53 -0600

    game health bar, attack, special attack, heal, implemented

---
+++ section4_course_project_the monster_slayer_game/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,71 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+            winner: null,
+        }
+    },
+    watch: {
+        playerHealth(value){
+            if(value <= 0 &&  this.monsterHealth <=0){
+                this.winner = 'draw';
+            } else if (value<=0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value){
+            if(value <= 0 &&  this.playerHealth <=0){
+                this.winner = 'draw';
+            } else if (value<=0) {
+                this.winner = 'player';
+            }
+        }
+    },
+    computed: {
+        monsterBarStyles(){
+            return {width: this.monsterHealth + '%'};
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !== 0;
+        }
+    },
+    methods: {
+        attackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(5,15);
+            this.monsterHealth -= attackValue; 
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(10,15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(15,20);
+            this.monsterHealth -= attackValue; 
+            this.attackPlayer();
+        },
+        healPlayer(){
+            this.currentRound++;
+            const healValue = getRandomValue(10,20);
+            if(this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+        }
+    },
+})
+
+app.mount('#game')
